8b01910d28c73a4c3fb635813d232f72832f3532
temitope-israel <omoniyitemitopeisrael@gmail.com>
2021-06-03 00:10:32 +0100

    checking to see if commit work

--- storage.js
+++ storage.js
@@ -1,19 +1,18 @@
 class Storage {
-    constructor() {
-        this.city;
-        this.defaultCity = 'lagos'
-    };
+  constructor() {
+    this.city;
+    this.defaultCity = "lagos";
+  }
 
-    getLastLocation() {
-        if(localStorage.getItem('city') === null) {
-            this.city = this.defaultCity;
-        }
-        else {
-            this.city = localStorage.getItem('city')
-        }
-        return {city: this.city}
+  getLastLocation() {
+    if (localStorage.getItem("city") === null) {
+      this.city = this.defaultCity;
+    } else {
+      this.city = localStorage.getItem("city");
     }
-    setLastLocation(city) {
-        localStorage.setItem('city', city)
-    }
+    return { city: this.city };
+  }
+  setLastLocation(city) {
+    localStorage.setItem("city", city);
+  }
 }
